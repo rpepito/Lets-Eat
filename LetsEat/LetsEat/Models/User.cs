07f5aaeecb0af9212784b210ab47ffb378240372
Ryan Pepito <pepitor@unlv.nevada.edu>
2018-02-16 12:31:57 -0800

    Base framework established, login page needs work. -Ryan

---
+++ LetsEat/LetsEat/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetsEat.Models
+{
+    public class User
+    {
+        // Variables that will hold the user's information
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        // Empty constructor for user class
+        public User() { }
+
+        // Function
+        public User(string UserName, string Password)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+        }
+    }
+}
