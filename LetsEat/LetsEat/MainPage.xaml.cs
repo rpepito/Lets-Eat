d13d9580b7aef088d8d00967b95e8e90d9f20ed7
Ryan Pepito <pepitor@unlv.nevada.edu>
2018-02-19 18:42:56 -0800

    Implemented restaurant and login view

--- LetsEat/LetsEat/MainPage.xaml.cs
+++ LetsEat/LetsEat/MainPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//Pull the current class/files from the root
+using LetsEat.Models;
+using LetsEat.Views;
+using LetsEat.Views.Customer_Side;
+
+//Default stuff
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +13,7 @@ using Xamarin.Forms;
 
 namespace LetsEat
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : MasterDetailPage
 	{
 		public MainPage()
 		{
---
+++ LetsEat/LetsEat/Models/MasterPageItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetsEat.Models
+{
+    pubic class MasterPageItem
+    {
+        //Sets title of master page item
+        public string Title { get; set; }
+
+        //Sets target to be pulled for use for master detail item
+        public Type TargetType { get; set; }
+    }
+}
