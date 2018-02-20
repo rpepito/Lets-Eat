21d83c1006210d1555302a7a4edb1b4dc87104d4
Ryan Pepito <pepitor@unlv.nevada.edu>
2018-02-19 20:49:18 -0800

    Implemented blank owner page. To gain access to owner page, must use hardcoded values: -Username: owner -Password: owner123

--- LetsEat/LetsEat/Views/Login.xaml.cs
+++ LetsEat/LetsEat/Views/Login.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using LetsEat.Models;
+using LetsEat.Views;
+using LetsEat.Views.Owner_Side;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,13 +20,20 @@ namespace LetsEat.Views
 			InitializeComponent ();
 		}
 
-        void SignInProcedure (object sender, EventArgs e)
+        private void SignInProcedure (object sender, EventArgs e)
         {
             User user = new User(UsrName_Entry.Text, Pwd_Entry.Text);
             if (user.UserCheckInfo())
-                DisplayAlert("Sign In", "Sign In Successful.", "Okay");                                         //If sign in successful, display message
+                if (UsrName_Entry.Text.Equals("owner") && Pwd_Entry.Text.Equals("owner123"))
+                    OwnerPageTransfer();                                                                            //if the textbox has the hardcoded values, call the owner page!
+                else
+                    DisplayAlert("Sign In", "Sign In Successful.", "Okay");                                         //If sign in successful, display message
             else
                 DisplayAlert("Sign In", message: "Error: Empty or Invalid Username and/or Password.", cancel: "Okay");     //If user enters a blank on either fields (or both) display error
         }
-	}
+
+        private async void RegisterProcedure(object sender, EventArgs e) => await Navigation.PushAsync(new Registration());
+        private async void OwnerPageTransfer() => await Navigation.PushAsync(new OwnerPage());
+
+    }
 }
---
+++ LetsEat/LetsEat/Views/Registration.xaml.cs
@@ -0,0 +1,21 @@
+﻿using LetsEat.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LetsEat.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Registration : ContentPage
+	{
+        public Registration()
+        {
+            InitializeComponent();
+        }
+	}
+}
