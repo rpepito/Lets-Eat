d0b536c2e1b715674b6baeb7dc8a04b6b6e3815c
rpepito <pepitor@unlv.nevada.edu>
2018-03-26 11:41:12 -0700

    new updated project files. login improperly working, will update issues page soon.

---
+++ LetsEat/Models/MasterPageItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LetsEat.Models
+{
+    public class MasterPageItem
+    {
+        // Sets title of master page item
+        public string Title { get; set; }
+
+        // Sets target to be pulled for use for master detail item
+        public Type TargetType { get; set; }
+    }
+}
---
+++ LetsEat/obj/Debug/android/src/md5cce693907115afaff50af31afdc20eb5/LoginActivity.java
@@ -0,0 +1,47 @@
+package md5cce693907115afaff50af31afdc20eb5;
+
+
+public class LoginActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("LetsEat.Views.Log_In.LoginActivity, LetsEat, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", LoginActivity.class, __md_methods);
+	}
+
+
+	public LoginActivity ()
+	{
+		super ();
+		if (getClass () == LoginActivity.class)
+			mono.android.TypeManager.Activate ("LetsEat.Views.Log_In.LoginActivity, LetsEat, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
