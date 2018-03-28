dc42f12cf3b9e441f6e760abaa9de58cc9ad541d
Adrian San Pedro <adrian.r.sanpedro@gmail.com>
2018-03-28 15:14:30 -0700

    Added Firebase Authentication functionality and Related Packages; Added Login and Registration cs files;

--- LetsEat/MainActivity.cs
+++ LetsEat/MainActivity.cs
@@ -1,18 +1,44 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Firebase;
+using Firebase.Auth;
+using static Android.Views.View;
+using Android.Support.Design.Widget;
+using Android.Gms.Tasks;
+using Android.Content;
 
 namespace LetsEat
 {
     [Activity(Label = "LetsEat", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        public static FirebaseApp app;
+        FirebaseAuth auth;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.LoginLayout);
+            SetContentView(Resource.Layout.Main);
+
+            //Init Firebase  
+            InitFirebaseAuth();
+
+            //Adrian 03/28/2018 TODO: Remove below later after editing MainActivity.cs to work with LoginActivity.cs
+            StartActivity(new Intent(this, typeof(Views.Log_In.LoginActivity)));    
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder()
+              .SetApplicationId("1:100312271278:android:3d79789aa8f833cd")
+              .SetApiKey("AIzaSyAH9j_K2-wcnrmxj9hEarZaFvCh6dMsOWw")
+              .Build();
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options);
+            auth = FirebaseAuth.GetInstance(app);
         }
     }
 }
