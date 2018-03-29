3a7fcbb5d11c2afe719a367d613073102c8013ec
pete210339 <s.tanthma@gmail.com>
2018-03-29 16:45:47 -0700

    Add Restaurant Lists Will move them into customer folder later

--- LetsEat/MainActivity.cs
+++ LetsEat/MainActivity.cs
@@ -1,18 +1,34 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using static Android.Views.View;
+using Android.Content;
+using System.Collections.Generic;
 
 namespace LetsEat
 {
     [Activity(Label = "LetsEat", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        private List<Restaurant> mItems;
+        private ListView mListView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.LoginLayout);
+            SetContentView(Resource.Layout.Main);
+
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+            mItems = new List<Restaurant>();
+            mItems.Add(new Restaurant() { Name = "King and I Thai Restaurant", Cuisine = "Thai", Price = "$$" });
+            mItems.Add(new Restaurant() { Name = "Hot and Juicy Seafood", Cuisine = "Seafood", Price = "$$$" });
+            mItems.Add(new Restaurant() { Name = "I Love Sushi", Cuisine = "Japanese", Price = "$$$" });
+
+            MyListViewAdapter adapter = new MyListViewAdapter(this, mItems);
+            mListView.Adapter = adapter;
         }
     }
 }
--- LetsEat/obj/Debug/android/src/letseat/letseat/R.java
+++ LetsEat/obj/Debug/android/src/letseat/letseat/R.java
@@ -11,14 +11,19 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static final int loginButton=0x7f040002;
-        public static final int password=0x7f040001;
-        public static final int registerButton=0x7f040003;
-        public static final int userName=0x7f040000;
+        public static final int loginButton=0x7f040005;
+        public static final int myListView=0x7f040007;
+        public static final int password=0x7f040004;
+        public static final int registerButton=0x7f040006;
+        public static final int txtCuisine=0x7f040001;
+        public static final int txtName=0x7f040000;
+        public static final int txtPrice=0x7f040002;
+        public static final int userName=0x7f040003;
     }
     public static final class layout {
-        public static final int loginlayout=0x7f020000;
-        public static final int main=0x7f020001;
+        public static final int listview_row=0x7f020000;
+        public static final int loginlayout=0x7f020001;
+        public static final int main=0x7f020002;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
