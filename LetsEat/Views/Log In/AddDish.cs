1ea34ceccac1130ef4bd234dfc74602c0e051f6d
rpepito <pepitor@unlv.nevada.edu>
2018-04-06 15:11:50 -0700

    Re-upload
    reupload of Markus' lost branch, found it in Neverland.

---
+++ LetsEat/Views/Log In/AddDish.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LetsEat.Views.OwnerSide
+{
+    [Activity(Label = "AddDish")]
+    public class AddDish : Activity
+    {
+
+        Button btn_save;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.AddDishLayout);
+            // Create your application here
+
+            btn_save = FindViewById<Button>(Resource.Id.savebtn);
+
+            btn_save.Click += delegate {
+
+                Toast.MakeText(this, "Dish information as been saved!", ToastLength.Short).Show();
+
+            };
+        }
+
+    }
+}
---
+++ LetsEat/Views/Log In/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LetsEat.Views.Owner_Side
+{
+    public class Dish
+    {
+
+        public string Dish_Name { get; set; }
+
+        /*
+        public string Dish_Desc { get; set; }
+        public string Dish_String { get; set; }
+
+        public int Dish_Price { get; set; }
+        public int Dish_CC { get; set; }        //CC == Calorie Count
+        */
+    }
+}
