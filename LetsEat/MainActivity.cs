32061cfc8b4814f8db368626a22f15b4a074d066
Sirre <estein122@yahoo.com>
2018-04-07 19:43:35 -0700

    Add files via upload

--- LetsEat/MainActivity.cs
+++ LetsEat/MainActivity.cs
@@ -1,18 +1,44 @@
 ﻿using Android.App;
-using Android.Widget;
+using Android.Content;
+using Android.Gms.Tasks;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Widget;
+using Firebase;
+using Firebase.Auth;
+using static Android.Views.View;
 
 namespace LetsEat
 {
     [Activity(Label = "LetsEat", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        public static FirebaseApp app;
+        FirebaseAuth auth;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.LoginLayout);
+            //SetContentView(Resource.Layout.Main);
+
+            //Init Firebase  
+            InitFirebaseAuth();
+
+            //Adrian 03/28/2018 TODO: Remove below later after editing MainActivity.cs to work with LoginActivity.cs
+            StartActivity(typeof(Views.Owner_Side.OwnerPage));    
+        }
+
+        private void InitFirebaseAuth()
+        {
+            var options = new FirebaseOptions.Builder()
+              .SetApplicationId("1:100312271278:android:3d79789aa8f833cd")
+              .SetApiKey("AIzaSyAH9j_K2-wcnrmxj9hEarZaFvCh6dMsOWw")
+              .Build();
+            if (app == null)
+                app = FirebaseApp.InitializeApp(this, options);
+            auth = FirebaseAuth.GetInstance(app);
         }
     }
 }
---
+++ LetsEat/Views/Owner Side/OwnerPage.cs
@@ -0,0 +1,69 @@
+﻿using LetsEat.Models;
+using LetsEat.Views;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.Design.Widget;
+using Android.Gms.Tasks;
+using static Android.Views.View;
+
+namespace LetsEat.Views.Owner_Side
+{
+    [Activity(Label = "OwnerPage")]
+    public class OwnerPage : Activity, IOnClickListener
+    {
+        private Button btn_Menu, btn_Table, btn_Queue, btn_Reservation;
+        private RelativeLayout activity_main;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.OwnerPage);
+
+            btn_Menu = FindViewById<Button>(Resource.Id.Menubutton);
+            btn_Table = FindViewById<Button>(Resource.Id.Tablebutton);
+            btn_Queue = FindViewById<Button>(Resource.Id.Queuebutton);
+            btn_Reservation = FindViewById<Button>(Resource.Id.Reservationbutton);
+            activity_main = FindViewById<RelativeLayout>(Resource.Id.activity_main);
+
+            btn_Menu.SetOnClickListener(this);
+            btn_Queue.SetOnClickListener(this);
+            btn_Table.SetOnClickListener(this);
+            btn_Reservation.SetOnClickListener(this);
+        }
+
+        public void OnClick(View v)
+        {
+            if (v.Id == Resource.Id.Menubutton)
+            {
+                StartActivity(typeof(Menu));
+            }
+
+            else if(v.Id == Resource.Id.Tablebutton)
+            {
+                StartActivity(typeof(TableList));
+            }
+
+            else if (v.Id == Resource.Id.Queuebutton)
+            {
+                StartActivity(typeof(Queue));
+            }
+
+            else if (v.Id == Resource.Id.Reservationbutton)
+            {
+                StartActivity(typeof(Reservations));
+            }
+        }                
+    }
+}
