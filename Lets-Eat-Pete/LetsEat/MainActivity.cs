12a9c36806ac1ee8278f7bf2833c561a68c52bc8
Sirre <estein122@yahoo.com>
2018-04-20 12:39:24 -0700

    Add files via upload
    Added reservation functionality

---
+++ Lets-Eat-Pete/LetsEat/MainActivity.cs
@@ -0,0 +1,133 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using static Android.Views.View;
+using Android.Content;
+using System.Collections.Generic;
+
+namespace LetsEat
+{
+    [Activity(Label = "Let's Eat", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        ListView myList;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            myList = FindViewById<ListView>(Resource.Id.listView);
+
+            myList.Adapter = new MyCustomListAdapter(UserData.Users);
+
+            /*myList.ItemClick += delegate
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "King and I");
+                intent.PutExtra("Cuisine", "Thai");
+                StartActivity(intent);
+                
+            };*/
+
+            myList.ItemClick += MyList_ItemClick;
+        }
+
+        private void MyList_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+
+            if (e.Position == 0)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Aloha Kitchen");
+                intent.PutExtra("Cuisine", "Hawaiian");
+                intent.PutExtra("Photo", "images/hawaiian.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 1)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Lago - Bellagio");
+                intent.PutExtra("Cuisine", "Italian");
+                intent.PutExtra("Photo", "images/italian.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 2)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Lemongrass - Aria");
+                intent.PutExtra("Cuisine", "Thai & Chinese");
+                intent.PutExtra("Photo", "images/thai.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 3)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Olive Garden");
+                intent.PutExtra("Cuisine", "Italian");
+                intent.PutExtra("Photo", "images/italian2.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 4)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Pho Thanh Huong");
+                intent.PutExtra("Cuisine", "Vietnamese");
+                intent.PutExtra("Photo", "images/vietnamese.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 5)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Roberto's Taco Shop");
+                intent.PutExtra("Cuisine", "Mexican");
+                intent.PutExtra("Photo", "images/mexican.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 6)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "SW Steakhouse");
+                intent.PutExtra("Cuisine", "Steak");
+                intent.PutExtra("Photo", "images/steak.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 7)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Sweet Poke");
+                intent.PutExtra("Cuisine", "Sushi");
+                intent.PutExtra("Photo", "images/sushi.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 8)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Twist by Pierre Gagnaire");
+                intent.PutExtra("Cuisine", "French");
+                intent.PutExtra("Photo", "images/french.png");
+                StartActivity(intent);
+            }
+
+            if (e.Position == 9)
+            {
+                Intent intent = new Intent(this, typeof(RestaurantPage));
+                intent.PutExtra("Name", "Zuma Restaurant");
+                intent.PutExtra("Cuisine", "Japanese");
+                intent.PutExtra("Photo", "images/japanese.png");
+                StartActivity(intent);
+            }
+        }
+    }
+}
+
---
+++ Lets-Eat-Pete/LetsEat/ReservePage.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace LetsEat
+{
+    [Activity(Label = "ReservePage")]
+    public class ReservePage : ListActivity
+    {
+        //public static readonly List<string> reserveNames = new List<string>();
+        //
+        EditText reservationText;
+        Button reserveButton;
+        //ListView reserveList;
+        string reserveHolder = string.Empty;
+        string errMessage = "Reservation could not be made: Empty Name";
+        string confirmMessage = "Reservation successful!";
+        //static readonly List<string> reserveNames = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.ReservePageLayout);
+
+            var reserveNum = Intent.GetStringExtra("reserve_time");
+            var reserveNames = Intent.Extras.GetStringArrayList("list_reservations") ?? new string[0];
+            this.ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, reserveNames);
+
+            reservationText = FindViewById<EditText>(Resource.Id.ReservationText);
+            reserveButton = FindViewById<Button>(Resource.Id.ReservationButton);
+
+            //reserveList = FindViewById<ListView>(Resource.Id.ReserveList);
+            //*
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, reserveNames);
+            ListView listView = (ListView)FindViewById(Android.Resource.Id.List);
+            listView.Adapter = adapter;
+             //
+
+            reserveButton.Click += delegate
+            {
+                reserveHolder = reservationText.Text;
+                if (!(string.IsNullOrWhiteSpace(reserveHolder)))
+                {
+                    //reserveNames.Add(reserveHolder);
+                    Intent returnIntent = new Intent(this, typeof(RestaurantPage));
+                    returnIntent.PutExtra("list_return", reserveHolder);
+                    SetResult(Result.Ok, returnIntent);
+                    Toast.MakeText(ApplicationContext, confirmMessage + " Time: " + reserveNum, ToastLength.Long).Show();
+                    Finish();
+                }
+                else {
+                    Toast.MakeText(ApplicationContext, errMessage, ToastLength.Long).Show();
+                }
+
+            };
+            //*/
+        }
+    }
+}
