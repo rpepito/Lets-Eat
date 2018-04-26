d0501c2fbf9a9b0db64fc0a2de739996d1142147
asleepu <leej254@unlv.nevada.edu>
2018-04-26 03:09:12 -0700

    added page for queue
    added a semi-working queue - need to connect with database, add back button, and fix remove from queue and more

--- LetsEat/Views/Owner Side/Queue.cs
+++ LetsEat/Views/Owner Side/Queue.cs
@@ -10,12 +10,17 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Support.Design.Widget;
+using LetsEat.Views.Owner_Side;
 
 namespace LetsEat
 {
     [Activity(Label = "Queue")]
-    public class Queue : Activity
+    public class Queue : ListActivity
     {
+        
+        public static readonly List<string> queueNames = new List<string>();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,6 +28,32 @@ namespace LetsEat
             SetContentView(Resource.Layout.QueueLayout);
 
             // Create your application here
+            Button buttonAddToQueue, buttonRemoveFromQueue;
+            
+            buttonAddToQueue = FindViewById<Button>(Resource.Id.addToQueueButton);
+            buttonRemoveFromQueue = FindViewById<Button>(Resource.Id.removeFromQueueButton);
+
+            ListView  queueList = FindViewById<ListView>(Resource.Id.QueueList);
+            ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.ResListItem, queueNames);
+            queueList.Adapter = ListAdapter;
+
+            buttonAddToQueue.Click += (sender, e) =>
+            {
+                // move owner to a page where they can put in the name for the first person in line
+                Toast.MakeText(this, "Adding customer to queue..", ToastLength.Long).Show();
+                StartActivity(typeof(QueuePageActivity));
+            };
+            // gotta connect both buttons with database or make a variable not sure yet 
+            // not working yet
+            buttonRemoveFromQueue.Click += (sender, e) =>
+            {
+                Toast.MakeText(this, "Removed customer from queue", ToastLength.Long).Show();
+            };
+
+
         }
+
+       
+
     }
 }
---
+++ LetsEat/Views/Owner Side/QueuePageActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace LetsEat.Views.Owner_Side
+{
+    [Activity(Label = "Queue")]
+    public class QueuePageActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.QueuePage);
+
+            EditText queueText = FindViewById<EditText>(Resource.Id.QueueText);
+            Button addNameToQueueButton = FindViewById<Button>(Resource.Id.AddNameToQueueButton);
+
+            string queueHolder = string.Empty;
+            addNameToQueueButton.Click += (sender, e) =>
+            {
+                queueHolder = queueText.Text;
+                //if text entry is not blank, put in array and then go back to queue page
+                if (!(string.IsNullOrWhiteSpace(queueHolder)))
+                {
+                    Queue.queueNames.Add(queueHolder);
+                    addNameToQueueButton.Enabled = true;
+                }
+
+                StartActivity(typeof(Queue));
+            };
+        }
+    }
+}
