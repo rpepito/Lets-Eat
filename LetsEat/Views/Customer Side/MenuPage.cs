c17b4d3ff85135aef56eaf959554274c390dd3c9
pete210339 <s.tanthma@gmail.com>
2018-05-01 22:26:15 -0700

    add menu page for customer

--- LetsEat/Views/Customer Side/MenuPage.cs
+++ LetsEat/Views/Customer Side/MenuPage.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using LetsEat.Views.Owner_Side;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,52 +8,42 @@ using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
+using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
 using Firebase;
-using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Xamarin.Database;
 using Firebase.Xamarin.Database.Query;
-using LetsEat.Views.Owner_Side;
 
 namespace LetsEat
 {
     [Activity(Label = "MenuPage")]
-    public class MenuPage : Android.Support.V4.App.Fragment
-
+    public class MenuPage : Activity
     {
-        Button add_item_btn;
+        //Firebase URL
+        private const string FBURL = "https://fir-database-ec02e.firebaseio.com/";
         ListView menulistView;
         List<string> listDishes = new List<string>();
 
-        private FirebaseUser user;
-        private const string FBURL = "https://fir-database-ec02e.firebaseio.com/";
-
-        public async override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            user = FirebaseAuth.GetInstance(MainActivity.app).CurrentUser;
+            SetContentView(Resource.Layout.MenuList);
+            menulistView = FindViewById<ListView>(Resource.Id.menulistViewCustomer);
             await LoadDish_Data();
-            // Create your application here
-        }
+            //ArrayAdapter<string> ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.DishList, listDishes);
 
-        public static MenuPage NewInstance()
-        {
-            var frag1 = new MenuPage { Arguments = new Bundle() };
-            return frag1;
         }
 
         public async Task LoadDish_Data()
         {
-
             var firebase = new FirebaseClient(FBURL);
 
             var items = await firebase
                 .Child("menus")
-                .Child(user.Uid)
+                .Child("ETOxeKqaLScRYZO3tn9ceNwDhKf2")
                 .OnceAsync<Dish>();
 
             foreach (var item in items)
@@ -64,36 +54,10 @@ namespace LetsEat
                 listDishes.Add(Dish_info.Name);
 
                 Console.WriteLine(Dish_info.Name);
-
             };
 
-            var ListAdapter = new ArrayAdapter<string>(this.Activity, Resource.Layout.DishList, listDishes);
+            var ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.DishList, listDishes);
             menulistView.Adapter = ListAdapter;
-
         }
-
-        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
-
-            View view = inflater.Inflate(Resource.Layout.MenuList, null);
-
-            menulistView = view.FindViewById<ListView>(Resource.Id.menulistViewCustomer);
-
-
-            menulistView.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args)
-            {
-                Toast.MakeText(this.Activity, ((TextView)args.View).Text, ToastLength.Short).Show();
-            };
-
-            var ignored = base.OnCreateView(inflater, container, savedInstanceState);
-
-            return view;
-
-
-        }
-
-
     }
 }
--- LetsEat/obj/Debug/android/src/md5112e1ce544e292e139446fe091803014/MenuPage.java
+++ LetsEat/obj/Debug/android/src/md5112e1ce544e292e139446fe091803014/MenuPage.java
@@ -2,7 +2,7 @@ package md5112e1ce544e292e139446fe091803014;
 
 
 public class MenuPage
-	extends android.support.v4.app.Fragment
+	extends android.app.Activity
 	implements
 		mono.android.IGCUserPeer
 {
@@ -11,7 +11,6 @@ public class MenuPage
 	static {
 		__md_methods = 
 			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
-			"n_onCreateView:(Landroid/view/LayoutInflater;Landroid/view/ViewGroup;Landroid/os/Bundle;)Landroid/view/View;:GetOnCreateView_Landroid_view_LayoutInflater_Landroid_view_ViewGroup_Landroid_os_Bundle_Handler\n" +
 			"";
 		mono.android.Runtime.register ("LetsEat.MenuPage, LetsEat, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MenuPage.class, __md_methods);
 	}
@@ -31,14 +30,6 @@ public class MenuPage
 	}
 
 	private native void n_onCreate (android.os.Bundle p0);
-
-
-	public android.view.View onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2)
-	{
-		return n_onCreateView (p0, p1, p2);
-	}
-
-	private native android.view.View n_onCreateView (android.view.LayoutInflater p0, android.view.ViewGroup p1, android.os.Bundle p2);
 
 	private java.util.ArrayList refList;
 	public void monodroidAddReference (java.lang.Object obj)
