a4a123d37bca1f271ee1e5af533fa44145d6effd
asleepu <leej254@unlv.nevada.edu>
2018-05-03 10:58:27 -0700

    added queue functionality

--- LetsEat/Views/Owner Side/Queue.cs
+++ LetsEat/Views/Owner Side/Queue.cs
@@ -14,15 +14,30 @@ using Android.Widget;
 namespace LetsEat
 {
     [Activity(Label = "Queue")]
-    public class Queue : Activity
+    public class Queue : ListActivity
     {
+        public static readonly List<string> queueNames = new List<string>();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.QueueLayout);
+            SetContentView(Resource.Layout.CustomerQueueLayout);
 
             // Create your application here
+
+            ListView queueList = FindViewById<ListView>(Resource.Id.QueueList);
+            ListAdapter = new ArrayAdapter<string>(this, Resource.Layout.QueueListItem, queueNames);
+            queueList.Adapter = ListAdapter;
+
+            Button addToQueueButton = FindViewById<Button>(Resource.Id.AddNameToQueueButton);
+            addToQueueButton.Click += (sender, e) =>
+            {
+                Toast.MakeText(this, "Adding name to queue", ToastLength.Short).Show();
+                StartActivity(typeof(QueuePageActivity));
+            };
+
+
         }
     }
 }
--- LetsEat/Views/Owner Side/queue_frag.cs
+++ LetsEat/Views/Owner Side/queue_frag.cs
@@ -1,27 +1,46 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 
-using Android.Support.V4.App;
-using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
 using Android.Views;
 using Android.Widget;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using Android.Support.Design.Widget;
+
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Xamarin.Database;
+using Firebase.Xamarin.Database.Query;
 
 namespace LetsEat.Views.OwnerSide
 {
     public class queue_frag : Android.Support.V4.App.Fragment
     {
-        public override void OnCreate(Bundle savedInstanceState)
+        private const string FBURL = "https://fir-database-ec02e.firebaseio.com/";
+      
+
+        // List<Views.OwnerSide.Queuedb> listQueue = new List<Views.OwnerSide.Queuedb>();
+
+        ListView listView;
+
+        Views.CustomerSide.QueueListViewAdapter adapter;
+        List<Queuedb> listQueue = new List<Queuedb>();
+
+
+        FirebaseUser user;
+
+        public override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your fragment here
+
+            user = FirebaseAuth.GetInstance(MainActivity.app).CurrentUser;
+
+            await LoadData();
         }
 
         public static queue_frag NewInstance()
@@ -30,13 +49,68 @@ namespace LetsEat.Views.OwnerSide
             return frag2;
         }
 
+        private async Task LoadData()
+        {
+            Console.WriteLine("Loading database...");
+            var firebase = new FirebaseClient(FBURL);
+            
+            var items = await firebase
+                .Child("queues")
+                .Child(user.Uid)
+                .OnceAsync<Queuedb>();
+            
+            adapter = null;
+
+            
+            foreach (var item in items)
+            {
+                Console.WriteLine("Loading items...");                  // ERROR SOMEWHERE IN THIS FUNCTION 5/2
+                Console.WriteLine(item.Object.name);
+                Queuedb queuedb = new Queuedb();            // FUNCTION + LINES = ERROR??? NULLEXCEPTION CRASH ERROR DUNNO FIX
+                Console.WriteLine("Check1");
+                queuedb.uid = item.Key;
+                Console.WriteLine("Check2");
+                queuedb.name = item.Object.name;
+                Console.WriteLine("Check3");
+                listQueue.Add(queuedb);
+                Console.WriteLine("Check4");
+
+            }
+            
+            
+
+            Console.WriteLine("Updating adapter...");
+            adapter = new Views.CustomerSide.QueueListViewAdapter(this, listQueue);
+            adapter.NotifyDataSetChanged();
+            listView.Adapter = adapter;
+            listView.TextFilterEnabled = true;
+           
+        }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
 
+            View view = inflater.Inflate(Resource.Layout.OwnerQueueLayout, null);
+
+            Button removeFromQueueBtn = view.FindViewById<Button>(Resource.Id.removeFromQueueButton);
+
+            removeFromQueueBtn.Click += delegate {
+               Toast.MakeText(this.Activity, "Removing from queue", ToastLength.Short).Show();
+            };
+           
+            listView = (ListView)view.FindViewById(Android.Resource.Id.List);
+            adapter = new CustomerSide.QueueListViewAdapter(this, listQueue);
+            adapter.NotifyDataSetChanged();
+            listView.Adapter = adapter;
+            listView.TextFilterEnabled = true;
+
             var ignored = base.OnCreateView(inflater, container, savedInstanceState);
-            return inflater.Inflate(Resource.Layout.QueueLayout, null);
+
+            return view;
         }
+        
     }
+    
 }
